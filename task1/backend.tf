c85b418e7c75c61246b0f91736caf3d77c565543
denys1953 <den25640@gmail.com>
2026-04-04 19:16:09 +0300

    refactor: update backend configuration in Terraform

--- task1/backend.tf
+++ task1/backend.tf
@@ -1,13 +1,16 @@
 terraform {
-  required_version = ">= 1.5.0"
-
   backend "s3" {
-    endpoint                    = "fra1.digitaloceanspaces.com"
-    bucket                      = "dzhumalo-bucket"
-    key                         = "terraform.tfstate"
-    region                      = "us-east-1"
+    bucket = "dzhumalo-bucket"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
+    endpoints = {
+      s3 = "https://fra1.digitaloceanspaces.com"
+    }
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_requesting_account_id  = true
     force_path_style            = true
   }
 }
